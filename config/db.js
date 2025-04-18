1f2cb748eabbd1d4d3448f9590258a83bec3dc31
Abhishek Yadav <monuy8830@gmail.com>
2025-04-18 12:19:32 +0530

    Refactor MongoDB connection code by removing deprecated options and commenting out old connection logic

--- config/db.js
+++ config/db.js
@@ -1,13 +1,26 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+//old mongo connection
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useFindAndModify: false,
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     });
+//     console.log("mongodb connection success!");
+//   } catch (err) {
+//     console.log("mongodb connection failed!", err.message);
+//   }
+// };
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useFindAndModify: false,
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
     });
     console.log("mongodb connection success!");
   } catch (err) {
