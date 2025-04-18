68155cc5dd71f6b9f095dd46576ab0cf83099ad5
Abhishek Yadav <monuy8830@gmail.com>
2025-04-18 14:58:20 +0530

    fix: update MongoDB URI and change currency to USD in .env file; downgrade mongoose version in package.json and package-lock.json style: uncomment and clean up database connection code in db.js and others.js chore: update .gitignore to properly ignore .env file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-//default mongo connection
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
@@ -15,18 +14,6 @@ const connectDB = async () => {
     console.log("mongodb connection failed!", err.message);
   }
 };
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("mongodb connection success!");
-//   } catch (err) {
-//     console.log("mongodb connection failed!", err.message);
-//   }
-// };
 
 module.exports = {
   connectDB,
